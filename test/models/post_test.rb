2e9c62e538b106bdc71749da834dca8ecd00a969
Tony Drake <t27duck@gmail.com>
2022-03-25 19:38:32 -0400

    Init commit with test

---
+++ test/models/post_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class PostTest < ActiveSupport::TestCase
+  
+  test "with ar insert" do
+    assert_difference "Post.count" do
+      Post.create(title: "post")
+    end
+  end
+  
+  test "with raw insert" do
+    assert_difference "Post.count" do
+      Post.connection.execute("INSERT INTO posts (title, created_at, updated_at) VALUES ('a post', date(), date())")
+    end
+  end
+end
